b5990bed291e6cefca07c149ee0af8fc013ed70e
Giau Nguyen <giaunguyen@MacBook-Air-de-Giau.local>
2024-10-18 10:59:08 +0200

    étape 4

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function sayHello(firstname){
+function sayHello(firstname,hour){
+    if (hour >= 18){
+        console.log("Bonsoir "+ firstname+"!")
+    }
+    else{
 let message = "Bonjour !"
 message = "Bonjour " + firstname + "!"
 console.log (message)
+    }
 } 
-sayHello("Beyonce")
+sayHello(`Beyonce`, 11);
+sayHello(`Beyonce`, 18);
+sayHello(`Beyonce`, 17);
